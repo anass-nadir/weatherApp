58cffab54236b43ff15b30d2fbb40eed47c3b191
anass nadir <anass.nadir@gmail.com>
2020-08-07 14:35:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,295 @@
+<template>
+  <div>
+    <Menu
+      :showMenu="showMenu"
+      :toggleMenu="toggleMenu"
+      :darkModeActive="darkModeActive"
+    />
+    <main
+      :class="[{ 'root__container-dark': darkModeActive }, 'root__container']"
+    >
+      <header
+        :class="[{ 'main__header-dark': darkModeActive }, 'main__header']"
+        v-if="loggedIn"
+      >
+        <div class="left__section">
+          <svg
+            @click="toggleMenu"
+            class="hamburger__icon"
+            id="Menu_Burger_Icon"
+            data-name="Menu Burger Icon"
+            viewBox="31.5 30 49.9 32"
+          >
+            <defs />
+            <rect
+              id="Rectangle_9"
+              width="49.9"
+              height="4"
+              :class="[
+                { 'hamburger__icon__fill-dark': darkModeActive },
+                'hamburger__icon__fill'
+              ]"
+              data-name="Rectangle 9"
+              rx="2"
+              transform="translate(31.5 58)"
+            />
+            <rect
+              id="Rectangle_10"
+              width="49.9"
+              height="4"
+              :class="[
+                { 'hamburger__icon__fill-dark': darkModeActive },
+                'hamburger__icon__fill'
+              ]"
+              data-name="Rectangle 10"
+              rx="2"
+              transform="translate(31.5 44)"
+            />
+            <rect
+              id="Rectangle_11"
+              width="49.9"
+              height="4"
+              :class="[
+                { 'hamburger__icon__fill-dark': darkModeActive },
+                'hamburger__icon__fill'
+              ]"
+              data-name="Rectangle 11"
+              rx="2"
+              transform="translate(31.5 30)"
+            />
+          </svg>
+        </div>
+
+        <h3 class="date__text">Today</h3>
+
+        <div class="mode-toggle__container">
+          <span class="mode-toggle__text">Light</span>
+
+          <label class="toggle-button__container">
+            <input
+              @click="modeToggleSwitch"
+              type="checkbox"
+              class="mode-toggle__input"
+            />
+            <span
+              :class="[
+                { 'mode-toggle__bg-checked': darkModeActive },
+                'mode-toggle__bg'
+              ]"
+            ></span>
+            <span
+              :class="[
+                { 'mode-toggle__circle-checked': darkModeActive },
+                'mode-toggle__circle'
+              ]"
+            ></span>
+          </label>
+
+          <span class="mode-toggle__text">Dark</span>
+        </div>
+      </header>
+      <div class="main-container__bg">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Menu from './components/Menu';
+  export default {
+    name: 'App',
+    components: {
+      Menu
+    },
+    data() {
+      return {
+        showMenu: false,
+        darkModeActive: false,
+        userEmail: '',
+        loggedIn: true
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.showMenu = !this.showMenu;
+      },
+      modeToggleSwitch() {
+        this.darkModeActive = !this.darkModeActive;
+      }
+    }
+  };
+</script>
+
+<style>
+  .root__container {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-flow: column;
+    transition: background 300ms linear;
+    background-image: linear-gradient(to top, #5ee7df 0%, #66a6ff 100%);
+  }
+
+  .root__container-dark {
+    background: linear-gradient(to bottom, #372865, #000000);
+  }
+  /*Header*/
+  .main__header {
+    background-color: #fff;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 0.25fr;
+    box-shadow: 0 0 2rem rgba(0, 0, 255, 0.1);
+    margin: 0;
+    min-height: 4rem;
+    align-items: center;
+    animation: slidedown 0.5s ease-in-out, fadein 1s ease-in-out;
+  }
+
+  .main__header-dark {
+    background-color: #2b244d;
+    color: white;
+  }
+
+  .toggle-button__container {
+    cursor: pointer;
+    position: relative;
+    margin: 0 0.75rem 0 0.25rem;
+    display: inline-flex;
+  }
+
+  .mode-toggle__container {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+  }
+
+  .mode-toggle__input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  .mode-toggle__bg {
+    height: 1rem;
+    width: 2rem;
+    border-radius: 0.5rem;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: inline-block;
+    transition: background-color 300ms linear;
+  }
+
+  .mode-toggle__circle {
+    height: 1.3rem;
+    width: 1.3rem;
+    background-color: #2b244d;
+    position: absolute;
+    top: -0.18rem;
+    border-radius: 50%;
+    box-shadow: 0 0 0 rgba(0, 0, 255, 0.5);
+    transition: left 300ms linear;
+    left: 0.2rem;
+  }
+
+  .mode-toggle__circle-checked {
+    background-color: white;
+    left: 1.5rem;
+  }
+
+  .mode-toggle__bg-checked {
+    background-color: #ff0070;
+  }
+
+  .mode-toggle__text {
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+  }
+
+  /*Content*/
+  .left__section {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+    max-width: 5rem;
+  }
+
+  .date__text {
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+    display: inline;
+    margin: 0.5rem 0;
+  }
+
+  /*SVGs*/
+  .hamburger__icon {
+    position: relative;
+    z-index: 50;
+    height: 1rem;
+    padding: 0.5rem 1.5rem;
+    margin-right: 1rem;
+    cursor: pointer;
+  }
+
+  .hamburger__icon__fill {
+    fill: #2b244d;
+  }
+
+  .hamburger__icon__fill-dark {
+    fill: #ffff;
+  }
+
+  /*
+================
+    Body
+================
+*/
+  .main-container__bg {
+    min-height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .copyright__text {
+    display: block;
+    text-align: center;
+  }
+  @media screen and (max-width: 959px) {
+    .main__header {
+      grid-template-columns: 1fr;
+      justify-items: center;
+      padding: 1rem 0;
+      min-height: 8rem;
+    }
+
+    .left__section {
+      justify-self: start;
+      max-width: none;
+      grid-template-columns: 0 5fr;
+      width: 100%;
+    }
+
+    .logo__icon {
+      margin: 0 auto;
+    }
+
+    .hamburger__icon {
+      margin-right: 0;
+    }
+
+    .menu-header {
+      display: flex;
+      flex-wrap: wrap;
+      flex-flow: column;
+      align-items: center;
+      justify-content: space-around;
+    }
+  }
+
+  .hamburger__icon__fill {
+    fill: #594e78;
+  }
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,226 @@
+<template>
+  <aside
+    :class="[
+      { 'side-menu__container-active': showMenu },
+      'side-menu__container'
+    ]"
+    @click="toggleMenu"
+  >
+    <nav
+      :class="[
+        {
+          'slide-menu-active': showMenu,
+          'slide-menu-active-dark': darkModeActive
+        },
+        'slide-menu'
+      ]"
+    >
+      <section :class="[{ 'menu-header-dark': darkModeActive }, 'menu-header']">
+        <span class="greeting__text">Welcome Back</span>
+        <div class="profile-image__container">
+          <img
+            src="https://www.jamf.com/jamf-nation/img/default-avatars/generic-user-purple.png"
+            alt="profile-image"
+            class="profile__image"
+          />
+        </div>
+        <div class="account-details">
+          <span class="name__text">{{ userEmail }}</span>
+          <span class="email__text">Free Plan</span>
+        </div>
+      </section>
+      <section class="menu-body">
+        <ul :class="[{ 'menu-links-dark': darkModeActive }, 'menu-links']">
+          <router-link
+            tag="li"
+            class="menu-link"
+            exact-active-class="link-active"
+            @click="toggleMenu"
+            to="/"
+            >Home</router-link
+          >
+          <router-link
+            class="menu-link"
+            exact-active-class="link-active"
+            @click="toggleMenu"
+            tag="li"
+            to="/add"
+            >Add City</router-link
+          >
+          <li class="menu-link">Logout</li>
+        </ul>
+      </section>
+      <section class="menu-footer">
+        <small class="copyright__text"
+          >Copyright © {{ new Date().getFullYear() }}</small
+        >
+      </section>
+    </nav>
+  </aside>
+</template>
+
+<script>
+  export default {
+    props: ['toggleMenu', 'showMenu', 'darkModeActive'],
+    data() {
+      return {
+        userEmail: 'anass.nadir@gmail.com'
+      };
+    }
+  };
+</script>
+
+<style>
+  .side-menu__container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    overflow: hidden;
+    pointer-events: none;
+    z-index: 50;
+  }
+
+  .side-menu__container-active {
+    pointer-events: auto;
+  }
+
+  .side-menu__container::before {
+    content: '';
+    cursor: pointer;
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #0c1066;
+    opacity: 0;
+    transition: opacity 300ms linear;
+    will-change: opacity;
+  }
+
+  .side-menu__container-active::before {
+    opacity: 0.3;
+  }
+  .slide-menu {
+    box-sizing: border-box;
+    transform: translateX(-200%);
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    min-height: 100vh;
+    width: 90%;
+    max-width: 26rem;
+    background-color: white;
+    box-shadow: 0 0 2rem rgba(0, 0, 255, 0.1);
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 2fr 4fr 1fr;
+    grid-gap: 1rem;
+    transition: transform 300ms linear;
+    will-change: transform;
+  }
+
+  .slide-menu-active {
+    transform: none;
+  }
+
+  .slide-menu-active-dark {
+    background-color: #2b244d;
+  }
+  .menu-header {
+    background: linear-gradient(to right, #00ff9b, #5f84fb);
+    display: grid;
+    grid-template-rows: 1fr 4fr;
+    grid-template-columns: 1fr 4fr;
+    grid-template-areas: 'greeting greeting' 'image details';
+    box-sizing: border-box;
+    width: 100%;
+    align-content: center;
+    color: white;
+    box-shadow: 0 0.5rem 2rem rgba(0, 0, 255, 0.2);
+  }
+
+  .menu-header-dark {
+    background: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+  }
+  .greeting__text {
+    grid-area: greeting;
+    font-size: 1.25rem;
+    letter-spacing: 0.15rem;
+    text-transform: uppercase;
+    margin-top: 1rem;
+    justify-self: center;
+    align-self: center;
+  }
+  .account-details {
+    grid-area: details;
+    display: flex;
+    flex-flow: column;
+    margin-left: 1rem;
+    align-self: center;
+  }
+
+  .name__text {
+    font-size: 1.15rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .email__text {
+    font-size: 0.9rem;
+    letter-spacing: 0.1rem;
+  }
+
+  .menu-body {
+    display: grid;
+    width: 100%;
+  }
+
+  .menu-links {
+    display: flex;
+    flex-flow: column;
+    list-style: none;
+  }
+  .menu-links-dark {
+    color: white;
+  }
+
+  .link-active {
+    color: #495cfc;
+    border-bottom: 1px solid #495cfc;
+  }
+
+  .menu-link {
+    outline: none;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin: 0.5rem 0;
+    cursor: pointer;
+  }
+
+  .profile-image__container {
+    grid-area: image;
+    margin-right: 0.5rem;
+    border-radius: 50%;
+    height: 4rem;
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    align-self: center;
+    margin-left: 2rem;
+  }
+
+  .profile__image {
+    max-width: 4rem;
+  }
+  @media only screen and (max-width: 300px) {
+    .slide-menu {
+      width: 100%;
+    }
+  }
+</style>
